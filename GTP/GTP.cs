29cae71cda30581843203e7a787fa8cc7068d901
yuito-it <yuito_it@outlook.jp>
2024-02-16 17:45:16 +0900

    :sparkles: GTP.exeの起動、終了メソッドを追加

---
+++ GTP/GTP.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+
+namespace GTP;
+
+public class GTP
+{
+    public Process? GTPProcess;
+
+    /// <summary>
+    /// Start GTP Process
+    /// </summary>
+    /// <param name="GTPPath">Path of GTP.exe</param>
+    /// <param name="Param">Cmd param</param>
+    public void Run(string GTPPath, string? Param)
+    {
+        if (Param == null)
+        {
+            GTPProcess = Process.Start(GTPPath);
+        }
+        else
+        {
+            GTPProcess = Process.Start(GTPPath, Param);
+        }
+    }
+    
+    public void Close()
+    {
+        GTPProcess?.Close();
+    }
+}
